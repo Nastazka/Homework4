6da67beb3304fc677308e7003b8bc8380a472f89
Anastasia <nastazka8@gmail.com>
2023-12-22 16:23:56 +0300

    добавила первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main() {
+        while (true) {
+            Console.Write("Enter number of 'q' to exit: ");
+            string input = Console.ReadLine();
+            if (input == "q") {
+                break;
+            }
+            int number;
+            if (int.TryParse(input, out number)) {
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                if (sum % 2 == 0)
+                {
+                    Console.WriteLine("[STOP]");
+                    break;
+                }
+            }
+            else {
+                Console.WriteLine("Incotrrect enter. Please, enter integer number or 'q'.");
+            }
+        }
+    }
+};
