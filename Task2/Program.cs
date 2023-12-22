7cd693156a16c0d6906df80dfd6b91734d4859f8
Anastasia <nastazka8@gmail.com>
2023-12-22 16:24:20 +0300

    добавила второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(100, 1000);
+                Console.Write(numbers[i] + " ");
+            }
+
+            int evenNumbersCount = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenNumbersCount++;
+                }
+            }
+
+            Console.WriteLine("The number of even numbers in the array: {0}", evenNumbersCount);
+        }
+    }
+};
